b08ecc4411df16f5439d42aaac92f2e8c6aa64b5
Alessio Giordano <dev@alessiogiordano.net>
2022-10-21 15:27:12 +0200

    Setup of the Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+// https://docs.swift.org/package-manager/PackageDescription/index.html
+
+import PackageDescription
+
+let package = Package(
+    name: "University Cloud Suite",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "AUXLibrary",
+            targets: ["AUXLibrary"]),
+        .executable(
+            name: "University Cloud Suite",
+            targets: ["AUXServer"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        //.package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "4.0.0")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "AUXLibrary",
+            dependencies: []),
+        .executableTarget(
+            name: "AUXServer",
+            dependencies: ["AUXLibrary"]),
+        .testTarget(
+            name: "AUXTests",
+            dependencies: ["AUXLibrary", "AUXServer"]),
+    ]
+)
